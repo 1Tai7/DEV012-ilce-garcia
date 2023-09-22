4fccc9c2cc768d6a0c9a27ba7b711248e83804bc
Ilce <ilsemilena1706@gmail.com>
2023-09-21 19:28:34 -0500

    agregue que el boton de borrar limpie los resutlados de li

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const {
   getAverageWordLength,
 } = analyzer;
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
 //declare una variable llamada textArea que es igual a un elemento de html de tipo textarea que contiene un nombre ..
 const textArea = document.querySelector("textarea[name='user-input']");
 //declare una variable llamada button que es igual a un elemento html que contiene un id ..
 const button = document.getElementById("reset-button");
+
 //declare una variable llamada liword.. que es un elemento html con un atributo data-testid que tiene un valor word-count
 const liWordCount = document.querySelector('[data-testid="word-count"]');
 const wordCount = liWordCount.textContent;
@@ -39,22 +41,34 @@ const liWordLengthAverage = document.querySelector(
 const wordLengthAverage = liWordLengthAverage.textContent;
 
 //tome la variable textArea y la estoy escuchando cada vez que escribo, borro o hago un espacio
-textArea.addEventListener("input", function () {
-  liWordCount.textContent = wordCount + "" + getWordCount(textArea.value);
+textArea.addEventListener("input", function tgb() {
+  //obtuve el valor de testarea y lo establesco en li
+  liWordCount.textContent =
+    wordCount + "\u2002\u2002\u2002" + getWordCount(textArea.value);
   liCharacterCount.textContent =
-    characterCount + "" + getCharacterCount(textArea.value);
+    characterCount + "\u2002\u2002\u2002" + getCharacterCount(textArea.value);
   liCharacterCountExcludingSpaces.textContent =
     characterCountExcludingSpaces +
-    "" +
+    "\u2002\u2002\u2002" +
     getCharacterCountExcludingSpaces(textArea.value);
-  liNumberCount.textContent = numberCount + "" + getNumberCount(textArea.value);
-  liNumberSum.textContent = numberSum + "" + getNumberSum(textArea.value);
+  liNumberCount.textContent =
+    numberCount + "\u2002\u2002\u2002" + getNumberCount(textArea.value);
+  liNumberSum.textContent =
+    numberSum + "\u2002\u2002\u2002" + getNumberSum(textArea.value);
   liWordLengthAverage.textContent =
-    wordLengthAverage + "" + getAverageWordLength(textArea.value);
+    wordLengthAverage +
+    "\u2002\u2002\u2002" +
+    getAverageWordLength(textArea.value);
 });
 
 //tome la variable button y la estoy escuchando cada vez que haga click y despues de la coma se va a ejecutar la funcion
 //que dentro de la funcion , tome la funcion textarea y su valor lo iguale a " "
 button.addEventListener("click", function () {
   textArea.value = "";
+  liWordCount.textContent = wordCount;
+  liCharacterCount.textContent = characterCount;
+  liCharacterCountExcludingSpaces.textContent = characterCountExcludingSpaces;
+  liNumberCount.textContent = numberCount;
+  liNumberSum.textContent = numberSum;
+  liWordLengthAverage.textContent = wordLengthAverage;
 });
